891db10c3ce9f01a389fcf22869a2f30db6fde6c
typist <1003659191@qq.com>
2024-03-13 05:05:26 +0000

    feat: update test.py

--- src/test.py
+++ src/test.py
@@ -1,3 +1,12 @@
 from service.ec import ECService
+from service.eia import EIAService
 
-ECService.fetch_and_store_sitemap_page_content()
+# 拉取sitemap中的所有网页并存储html到本地 正文到mongo
+# ECService.fetch_and_store_sitemap_page_content()
+
+# 拉取所有路由下的所有数据 存储到mongo
+# EIAService.recursive_fetch_and_store_data()
+# 拉取total-energy下的所有数据 存储到mongo
+# EIAService.recursive_fetch_and_store_data("total-energy")
+# 拉取所有路由的meta信息 存储到本地
+# EIAService.recursive_fetch_and_store_meta()
