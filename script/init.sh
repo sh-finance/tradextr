0b89bc479c0aabb0c1d2e260c337b5baed1ccf45
typist <1003659191@qq.com>
2024-06-03 03:04:41 +0000

    chore: remove pip mirror

--- script/init.sh
+++ script/init.sh
@@ -5,7 +5,7 @@ cd $workspace
 
 python3 -m venv .venv
 source .venv/bin/activate
-pip install -i https://pypi.tuna.tsinghua.edu.cn/simple -r requirements.txt
+pip install -r requirements.txt
 
 # install git hook
 pre-commit install
